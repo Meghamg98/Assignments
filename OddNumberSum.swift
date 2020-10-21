4d9189ac029247e600fca8e1a33db035438c250e
Megha M Gamskar <72864453+Meghamg98@users.noreply.github.com>
2020-10-21 20:09:37 +0530

    Add files via upload

---
+++ OddNumberSum.swift
@@ -0,0 +1,18 @@
+func oddNumberSum(inputNumber: UInt) {
+    var index = 1, sum = 0
+
+    if(inputNumber % 2 != 0) {
+        while(index <= inputNumber) {
+            sum = sum + index
+            print("For odd numbers till \(index) the summation is \(sum)")
+            index = index + 2
+        }
+        print("\nThe total sum is = \(sum)")
+    }
+    else
+    {
+        print("Please enter an Odd positive integer")
+    }
+}
+
+oddNumberSum(inputNumber: 7)
---
+++ RightJustify.swift
@@ -0,0 +1,13 @@
+func rightJustify(initialString: String) {
+   let spaceCount = 70 - initialString.count - 1
+   var indexCount = 0
+
+   while(indexCount < spaceCount) {
+       print(terminator: " ")
+       indexCount += 1
+   }
+
+   print(initialString)
+}
+
+rightJustify(initialString: "Megha")
