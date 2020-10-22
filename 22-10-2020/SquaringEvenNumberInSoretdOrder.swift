17e200959e19080aa10913d053f779098cb062c3
Megha M Gamskar <72864453+Meghamg98@users.noreply.github.com>
2020-10-22 16:37:55 +0530

    Update SquaringEvenNumberInSoretdOrder.swift

--- 22-10-2020/SquaringEvenNumberInSoretdOrder.swift
+++ 22-10-2020/SquaringEvenNumberInSoretdOrder.swift
@@ -23,3 +23,36 @@ func filteringNumbers(list: [Int]) -> [Int] {
 
 let elements=[ 2, 5, 3, 8, 10, 4, 7]
 print("The final list obtained :\(filteringNumbers(list: elements))")
+
+
+//Using Closure
+let filteringNumbersClosure = {
+    (list: [Int]) -> [Int]
+    in
+    
+    var resultList = [Int]()
+    var result = [Int]()
+    
+    
+    for (_, value) in list.enumerated() {
+        if(value % 2 == 0){
+            resultList.append(value)
+        }
+    }
+    print("Step 1: Filtering Even numbers \(resultList)")
+    
+    for (_, value) in resultList.enumerated() {
+        var square = 0
+        square = value * value
+        result.append(square)
+    }
+    print("Step 2: Sqauring Even numbers \(result)")
+    
+    result = result.sorted()
+    
+    
+    return result
+}
+
+let elements=[ 2, 5, 3, 8, 10, 4, 7]
+print("The final list obtained :\(filteringNumbersClosure(elements))")
