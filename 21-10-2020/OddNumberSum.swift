43593f2ef6304a0f175670af25b94b26fd874b37
Megha M Gamskar <72864453+Meghamg98@users.noreply.github.com>
2020-10-22 15:26:44 +0530

    Added Closure part

--- 21-10-2020/OddNumberSum.swift
+++ 21-10-2020/OddNumberSum.swift
@@ -1,3 +1,4 @@
+//Using Function
 func oddNumberSum(inputNumber: UInt) {
     var index = 1, sum = 0
 
@@ -16,3 +17,29 @@ func oddNumberSum(inputNumber: UInt) {
 }
 
 oddNumberSum(inputNumber: 7)
+
+
+
+
+//Using Closure
+let oddNumberSumClosure = {
+    (inputNumber: UInt)
+    in
+    
+    var index = 1, sum = 0
+
+    if(inputNumber % 2 != 0) {
+        while(index <= inputNumber) {
+            sum = sum + index
+            print("For odd numbers till \(index), the summation is \(sum)")
+            index = index + 2
+        }
+        print("\nThe total sum is = \(sum)")
+    }
+    else
+    {
+        print("Please enter an Odd positive integer")
+    }
+}
+
+oddNumberSumClosure(5)
