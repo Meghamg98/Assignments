941005f2756cd63b2ffc8a1bcf8bbb5ce3421020
Megha M Gamskar <72864453+Meghamg98@users.noreply.github.com>
2020-10-22 15:23:37 +0530

    Added Closure part

--- 21-10-2020/SongInDescendingOrder.swift
+++ 21-10-2020/SongInDescendingOrder.swift
@@ -1,3 +1,4 @@
+//Using Function
 func songInDescendingOrder(number: Int) {
     var descendingCount = number
     let bottles = "bottles of water"
@@ -11,3 +12,23 @@ func songInDescendingOrder(number: Int) {
 }
 
 songInDescendingOrder(number:99)
+
+
+
+//Using Closure
+let songInDescendingOrderClosure = {
+    (number: Int)
+    in
+    
+    var descendingCount = number
+    let bottles = "bottles of water"
+
+    while(descendingCount >= 1) {
+        print("\(descendingCount) \(bottles) on the wall,\n\(descendingCount) \(bottles),\nya’ take one down, ya’ pass it around,\n\(descendingCount-1) \(bottles) on the wall.\n")
+        descendingCount = descendingCount - 1
+    }
+    
+    print("No bottles of water on the wall,\nno bottles of water,\nya’ can’t take one down, ya’ can’t pass it around,\n’cause there are no more bottles of water on the wall!") 
+}
+
+songInDescendingOrderClosure(99)
