82f055ab3c915c2f5360ed72b9427fa62c2b2755
Megha M Gamskar <72864453+Meghamg98@users.noreply.github.com>
2020-10-22 15:15:23 +0530

    Added Closure part

--- 21-10-2020/RightJustify.swift
+++ 21-10-2020/RightJustify.swift
@@ -1,3 +1,4 @@
+//Using function
 func rightJustify(initialString: String) {
    let spaceCount = 70 - initialString.count - 1
    var indexCount = 0
@@ -11,3 +12,24 @@ func rightJustify(initialString: String) {
 }
 
 rightJustify(initialString: "Megha")
+
+
+
+
+//Using Closure
+let rightJustify = {
+    (initialString: String)
+    in
+    
+    let spaceCount = 70 - initialString.count - 1
+    var indexCount = 0
+
+    while(indexCount < spaceCount) {
+       print(terminator: " ")
+       indexCount += 1
+    }
+
+    print(initialString)
+}
+
+rightJustify( "Megha")
