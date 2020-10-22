c5660960cdb4e67ab8000c3fc0a244c48ede9b06
Megha M Gamskar <72864453+Meghamg98@users.noreply.github.com>
2020-10-22 15:18:53 +0530

    Added Closure

--- 21-10-2020/Fermat.swift
+++ 21-10-2020/Fermat.swift
@@ -1,3 +1,4 @@
+//Using Function
 func checkFermat(a: Int, b: Int, c: Int, n: Int) {
     let leftSide = a ^ n + b ^ n  
     let rightSide = c ^ n
@@ -11,3 +12,23 @@ func checkFermat(a: Int, b: Int, c: Int, n: Int) {
 }
 
 checkFermat(a: 2, b: 4, c: 2, n: 3)
+
+
+
+//Using Closure
+let checkFermatClosure =  {
+    (a: Int, b: Int, c: Int, n: Int)
+    in
+    
+    let leftSide = a ^ n + b ^ n  
+    let rightSide = c ^ n
+
+    if(n > 2 && leftSide == rightSide) {
+        print("Holy smokes, Fermat was wrong!")
+    }  
+    else {
+        print("No, that doesn’t work.")
+    }
+}
+
+checkFermatClosure(2, 4, 2, 3)
