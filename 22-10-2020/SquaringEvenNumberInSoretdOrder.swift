894e29372479f0772601d1184eaa05162e528704
Megha M Gamskar <72864453+Meghamg98@users.noreply.github.com>
2020-10-23 09:39:47 +0530

    Update SquaringEvenNumberInSoretdOrder.swift

--- 22-10-2020/SquaringEvenNumberInSoretdOrder.swift
+++ 22-10-2020/SquaringEvenNumberInSoretdOrder.swift
@@ -26,33 +26,31 @@ print("The final list obtained :\(filteringNumbers(list: elements))")
 
 
 //Using Closure
-let filteringNumbersClosure = {
-    (list: [Int]) -> [Int]
-    in
-    
+func squaringEvenNumbersInAscendingOrder(initialList: [Int], condition: (Int) -> Bool) -> [Int] {
     var resultList = [Int]()
-    var result = [Int]()
+    var square = 0
     
-    
-    for (_, value) in list.enumerated() {
-        if(value % 2 == 0){
-            resultList.append(value)
+    for(_, value) in initialList.enumerated() {
+        if(condition(value)) {
+            square = value * value
+            resultList.append(square)
         }
     }
-    print("Step 1: Filtering Even numbers \(resultList)")
+    resultList = resultList.sorted()
     
-    for (_, value) in resultList.enumerated() {
-        var square = 0
-        square = value * value
-        result.append(square)
-    }
-    print("Step 2: Sqauring Even numbers \(result)")
-    
-    result = result.sorted()
-    
-    
-    return result
+    return resultList
 }
 
-let elements=[ 2, 5, 3, 8, 10, 4, 7]
-print("The final list obtained :\(filteringNumbersClosure(elements))")
+let evenClosure = {
+    (value: Int) -> Bool
+    in
+    
+    if(value % 2 == 0)
+    {
+        return true
+    }
+    return false
+}
+
+let numbers = [2, 6, 3, 9, 1, 4, 7]
+print("The final list is \(squaringEvenNumbersInAscendingOrder(initialList: numbers, condition: evenClosure) )")
