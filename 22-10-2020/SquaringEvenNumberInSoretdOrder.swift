6637ab54e482e8bf9f624e8593e50db1e3600940
Megha M Gamskar <72864453+Meghamg98@users.noreply.github.com>
2020-10-23 15:38:48 +0530

    Optimizing the closure

--- 22-10-2020/SquaringEvenNumberInSoretdOrder.swift
+++ 22-10-2020/SquaringEvenNumberInSoretdOrder.swift
@@ -19,30 +19,24 @@ print("The final list obtained :\(squaringEvenNumbersInAscendingOrder(initialLis
 
 //Using Closure
 func squaringEvenNumbersInAscendingOrder(initialList: [Int], condition: (Int) -> Bool) -> [Int] {
-    var resultList = [Int]()
+    var list = [Int]()
     var square = 0
     
     for(_, value) in initialList.enumerated() {
         if(condition(value)) {
             square = value * value
-            resultList.append(square)
+            list.append(square)
         }
     }
-    resultList = resultList.sorted()
+    list = list.sorted()
     
-    return resultList
+    return list
 }
 
-let evenClosure = {
-    (value: Int) -> Bool
-    in
-    
-    if(value % 2 == 0)
-    {
-        return true
-    }
-    return false
+let numbers = [ 2, 5, 3, 8, 10, 4, 7]
+let finalList = squaringEvenNumbersInAscendingOrder(initialList: numbers) {
+
+    $0 % 2 == 0 ? true : false
 }
 
-let numbers = [ 2, 5, 3, 8, 10, 4, 7, 6]
-print("The final list is \(squaringEvenNumbersInAscendingOrder(initialList: numbers, condition: evenClosure) )")
+print("The final list is \( finalList)")
