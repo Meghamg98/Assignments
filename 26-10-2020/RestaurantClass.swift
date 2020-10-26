339b1a0acedfedf8d470810553682074f7cc626f
Megha M Gamskar <72864453+Meghamg98@users.noreply.github.com>
2020-10-26 22:18:52 +0530

    Create RestaurantClass.swift

---
+++ 26-10-2020/RestaurantClass.swift
@@ -0,0 +1,22 @@
+class Restaurant {
+    var name = "Ghee Rice"
+    var price = 200
+    var quantity = "Mini"
+    var numberOfOrders = 3
+    var isHomeDeliveryAvailable = true
+    
+}
+
+let myFood = Restaurant()
+
+print("-------My Food Order details------- ")
+print("Name\t \t: \(myFood.name) ")
+print("Price\t\t: \(myFood.price) ")
+print("Quantity  \t: \(myFood.quantity) ")
+print("Number of Orders: \(myFood.numberOfOrders) ")
+
+if(myFood.isHomeDeliveryAvailable == true) {
+    print("Home delivery is available to your adrress")
+} else {
+    print("Oh! we do not delivery to your address")
+}
