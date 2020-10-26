eb732bbf7ac16ef43bb0f76712d779c56661b751
Megha M Gamskar <72864453+Meghamg98@users.noreply.github.com>
2020-10-26 09:36:38 +0530

    Update SongInDescendingOrder.swift

--- 21-10-2020/SongInDescendingOrder.swift
+++ 21-10-2020/SongInDescendingOrder.swift
@@ -16,27 +16,3 @@ func songInDescendingOrder(number: Int) {
 }
 
 songInDescendingOrder(number:9)
-
-
-
-//Using Closure
-let songInDescendingOrderClosure = {
-    (number: Int)
-    in
-    
-    var descendingCount = number
-    let bottles = "bottles of water"
-
-     if(number<0) {
-        print("Please enter a positer Integer")
-    } else {
-        while(descendingCount >= 1) {
-            print("\(descendingCount) \(bottles) on the wall,\n\(descendingCount) \(bottles),\nya’ take one down, ya’ pass it around,\n\(descendingCount-1) \(bottles) on the wall.\n")
-            descendingCount = descendingCount - 1
-        }
-    
-        print("No bottles of water on the wall,\nno bottles of water,\nya’ can’t take one down, ya’ can’t pass it around,\n’cause there are no more bottles of water on the wall!") 
-    }
-}
-
-songInDescendingOrderClosure(99)
