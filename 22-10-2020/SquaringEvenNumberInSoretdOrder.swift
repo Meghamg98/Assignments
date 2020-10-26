09dc909ef05caf85150bbe7bd6b76573cfd89a55
Megha M Gamskar <72864453+Meghamg98@users.noreply.github.com>
2020-10-26 09:41:37 +0530

    Update SquaringEvenNumberInSoretdOrder.swift

--- 22-10-2020/SquaringEvenNumberInSoretdOrder.swift
+++ 22-10-2020/SquaringEvenNumberInSoretdOrder.swift
@@ -16,27 +16,3 @@ func squaringEvenNumbersInAscendingOrder(initialList: [Int]) -> [Int] {
 let elements=[ 2, 5, 3, 8, 10, 4, 7, 6]
 print("The final list obtained :\(squaringEvenNumbersInAscendingOrder(initialList: elements))")
 
-
-//Using Closure
-func squaringEvenNumbersInAscendingOrder(initialList: [Int], condition: (Int) -> Bool) -> [Int] {
-    var list = [Int]()
-    var square = 0
-    
-    for(_, value) in initialList.enumerated() {
-        if(condition(value)) {
-            square = value * value
-            list.append(square)
-        }
-    }
-    list = list.sorted()
-    
-    return list
-}
-
-let numbers = [ 2, 5, 3, 8, 10, 4, 7]
-let finalList = squaringEvenNumbersInAscendingOrder(initialList: numbers) {
-
-    $0 % 2 == 0 ? true : false
-}
-
-print("The final list is \( finalList)")
