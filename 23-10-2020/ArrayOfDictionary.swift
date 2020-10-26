fc472ee6fe4056900ba8ce7e46536bb6084b9670
Megha M Gamskar <72864453+Meghamg98@users.noreply.github.com>
2020-10-26 19:08:00 +0530

    Create ArrayOfDictionary.swift

---
+++ 23-10-2020/ArrayOfDictionary.swift
@@ -0,0 +1,43 @@
+var arrayOfDictionary = [[String: Any]]()
+var finalArray = [Any]()
+
+arrayOfDictionary = [
+    [
+      "Name": "ABC",
+      "Age": 20,
+      "Branch": "ECE"
+    ],
+    [
+      "Name": "DEF",
+      "Age": 21,
+      "Branch": "CS"
+    ],
+    [
+      "Name": "GHI",
+      "Age": 22,
+      "Branch": "IS"
+    ]
+]
+
+
+func arrayOfDictionaryFunction(array: [[String: Any]]) -> [Any] {
+    let keyArray = [String](arrayOfDictionary[0].keys)
+    
+        for j in 0..<array.count {
+            if let value = array[j][keyArray[0]] {
+                finalArray.append(value) 
+            }
+            if let value = array[j][keyArray[1]] {
+                finalArray.append(value) 
+            }
+            if let value = array[j][keyArray[2]] {
+                finalArray.append(value) 
+            }
+        
+    }
+    
+    return finalArray
+}
+
+let resultString = arrayOfDictionaryFunction(array: arrayOfDictionary)
+print(resultString)
