060a9b8406d961a6a155f1517fca6225d44bc623
Megha M Gamskar <72864453+Meghamg98@users.noreply.github.com>
2020-10-26 09:38:04 +0530

    Update RightJustify.swift

--- 21-10-2020/RightJustify.swift
+++ 21-10-2020/RightJustify.swift
@@ -12,24 +12,3 @@ func rightJustify(initialString: String) {
 }
 
 rightJustify(initialString: "Megha")
-
-
-
-
-//Using Closure
-let rightJustifyClosure = {
-    (initialString: String)
-    in
-    
-    let spaceCount = 70 - initialString.count - 1
-    var indexCount = 0
-
-    while(indexCount < spaceCount) {
-       print(terminator: " ")
-       indexCount += 1
-    }
-
-    print(initialString)
-}
-
-rightJustifyClosure("Megha")
