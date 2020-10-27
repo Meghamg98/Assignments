fd9b2bbc035f91d785aae1eac4504c3ab268163d
Megha M Gamskar <72864453+Meghamg98@users.noreply.github.com>
2020-10-27 13:33:23 +0530

    Update RestaurantClass.swift

--- 26-10-2020/RestaurantClass.swift
+++ 26-10-2020/RestaurantClass.swift
@@ -1,11 +1,9 @@
-//With single value
 class Restaurant {
     var name = "Ghee Rice"
     var price = 200
     var quantity = "Mini"
     var numberOfOrders = 3
     var isHomeDeliveryAvailable = true
-    
 }
 
 let myFood = Restaurant()
@@ -23,42 +21,3 @@ if(myFood.isHomeDeliveryAvailable == true) {
 }
 
 
-//With multiple values
-class Restaurant {
-    var name: String = ""
-    var price: Int = 0
-    var quantity: String = ""
-    var numberOfOrders: Int = 0
-    var isHomeDeliveryAvailable: Bool = true
-}
-
-let myFood = Restaurant()
-
-func myOrder() {
-    print("Name\t \t: \(myFood.name) ")
-    print("Price\t\t: \(myFood.price) ")
-    print("Quantity  \t: \(myFood.quantity) ")
-    print("Number of Orders: \(myFood.numberOfOrders) ")
-    
-    if(myFood.isHomeDeliveryAvailable == true) {
-        print("Home delivery is available to your adrress")
-    } else {
-        print("Oh! we do not delivery to your address")
-    }
-    
-}
-myFood.name = "Curd Rice"
-myFood.price = 200
-myFood.quantity = "Small"
-myFood.numberOfOrders = 4
-myFood.isHomeDeliveryAvailable = true
-print("-------My First Order details------- ")
-print(myOrder())
-
-myFood.name = "Noodles"
-myFood.price = 100
-myFood.quantity = "Large"
-myFood.numberOfOrders = 2
-myFood.isHomeDeliveryAvailable = false
-print("-------My Second Order details------- ")
-print(myOrder())
