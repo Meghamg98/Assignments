0667f1e15c1de88df0aa10913860131f8b3ec09c
Megha M Gamskar <72864453+Meghamg98@users.noreply.github.com>
2020-11-24 10:42:01 +0530

    Update BankProtocol.swift

--- 18-11-2020/BankProtocol.swift
+++ 18-11-2020/BankProtocol.swift
@@ -1,21 +1,25 @@
 import Foundation
 
 class AccountManager {
-	var generateAccountNumber = 6330678420000
-	var listOfAccounts: [Account]
-	
+	var generatedAccountNumber = 6330678420000
+	var listOfAccounts: [SendAccountManagerProtocol]
+    
 	init() {
-		self.listOfAccounts = [Account]()
+		self.listOfAccounts = [SendAccountManagerProtocol]()
 	}
 
 	func createAccountNumber(account: Account) -> Int {
-		generateAccountNumber += 1
-		print("\nHi \(account.name), your Account number is \(generateAccountNumber)")
-		return generateAccountNumber
+		generatedAccountNumber += 1
+		print("\nHi \(account.name), your Account number is \(generatedAccountNumber)")
+		return generatedAccountNumber
 	}
 
 	func addAccount(account: Account) {
 		self.listOfAccounts.append(account)
+	}
+
+	func queryCurrentBalance(account: Account) {
+		print("Current Balance: \(account.availableAmount())")
 	}
 
 	func sendAccountDetails(details: Account) {
@@ -23,10 +27,21 @@ class AccountManager {
 			acc.accountDetails(account: details)
 		}
 	}
+
+	func depositeAmount(account: Account, amount: Float) {
+		account.depositeAmount(amount: amount)
+	}
+
+	func withdrawAmount(account: Account, amount: Float) {
+		account.withdrawAmount(amount: amount)
+	}
 }
 
-protocol SendAccountMangerProtocol {
+protocol SendAccountManagerProtocol {
 	func accountDetails(account: Account)
+	func availableAmount() -> Float
+	func depositeAmount(amount: Float)
+	func withdrawAmount(amount: Float)
 }
 
 class Account {
@@ -37,7 +52,7 @@ class Account {
 	var mobileNumber: Int? = 0
 	var email: String? = nil
 	var accountNumber = 0
-	var transactions = [String: String]()
+	var transactions: String = " "
 	var transactionCount = 0
 
 	init(name: String, initialAmount: Float) {
@@ -58,47 +73,21 @@ class Account {
 		print("Dear \(self.name), your email \(email) is updated successfully")
 	}
 
-	func currentBalance()  {
+	func currentBalance() {
 		let interestRate: Float = 6.7
-        var finalAmount: Float
+		var finalAmount: Float
 		let fdTerm = 5
 
-        if(accountType == "SA" || accountType == "CA" || accountType == "RDA") {
-            finalAmount = currentAmount + ( currentAmount * interestRate / 365)
-            currentAmount = finalAmount
-        } else if(accountType == "FD") {
-            finalAmount = currentAmount + ((currentAmount * interestRate) / Float(365 * fdTerm))
-            currentAmount = finalAmount
-        }
+		if(accountType == "SA" || accountType == "CA" || accountType == "RDA") {
+			finalAmount = currentAmount + ( currentAmount * interestRate / 365)
+			currentAmount = finalAmount
+		} else if(accountType == "FD") {
+			finalAmount = currentAmount + ((currentAmount * interestRate) / Float(fdTerm))
+			currentAmount = finalAmount
+		}
 
 		print("The current balance of account \(self.accountNumber):- Rs. \(currentAmount)\n")
 	}
-
-	func depositeAmount(amount: Float) {
-		if (self.accountType == "CA" || self.accountType == "SA" || self.accountType == "RDA" || self.accountType == "HLA" || self.accountType == "VLA" || self.accountType == "PLA") {
-			transactionCount += 1
-			currentAmount += amount
-			transactions["Transaction\(transactionCount)"] = "+\(amount)"
-		
-			print("\nDear \(self.name),you have deposited Rs. \(amount). \nThe availble balance is Rs. \(currentAmount)")
-	    } else if(self.accountType == "FDA" ) {
-			print("Fixed deposit supports only one time deposition. You have already deposited Rs. \(initialAmount)")
-		} else {
-			print("Invalid account type")
-		}
-	} 
-
-	func withdrawAmount(amount: Float) {
-		if (self.accountType == "CA" || self.accountType == "SA") {
-			transactionCount += 1
-			currentAmount -= amount
-			transactions["Transaction\(transactionCount)"] = "-\(amount)"
-
-			print("\nDear \(self.name), you have withdrawed Rs. \(amount).\nThe avilable balance is Rs. \(currentAmount)")
-		} else {
-			print("\nCan not withdraw money from \(self.accountType) account ")
-		}
-	} 
 
 	func accountStatement() {
 		print("Transaction details for account number \(self.accountNumber) is as below:")
@@ -107,15 +96,55 @@ class Account {
 
 }
 
-extension Account: SendAccountMangerProtocol {
+extension Account: SendAccountManagerProtocol {
+
 	func accountDetails(account: Account) {
 		print("\n-----Account details of \(account.name)-----")
 		print("Name: \(account.name)")
+		if let mobile = account.mobileNumber {
+			print("Mobile Number: \(mobile)")
+		} else {
+			print("Mobile Number not updated")
+		}
+		if let eMail = account.email {
+			print("E-mail ID: \(eMail)")
+		}else {
+			print("E-mail ID not updated")
+		}
 		print("Account number: \(account.accountNumber)")
 		print("Account type: \(account.accountType)")
-		print("Current balance: \(account.currentAmount)\n")
+		print("Current balance: \(account.currentAmount)")
 	}
 
+	func availableAmount() -> Float {
+		return currentAmount
+	}
+
+	func depositeAmount(amount: Float) {
+		if (self.accountType == "CA" || self.accountType == "SA" || self.accountType == "RDA" || self.accountType == "HLA" || self.accountType == "VLA" || self.accountType == "PLA") {
+			transactionCount += 1
+			currentAmount += amount
+			transactions += "\nTransaction\(transactionCount)   Deposit         \(amount)"
+
+			print("\nDear \(self.name),you have deposited Rs. \(amount). \nThe availble balance is Rs. \(currentAmount)")
+		} else if(self.accountType == "FDA" ) {
+			print("Fixed deposit supports only one time deposition. You have already deposited Rs. \(initialAmount)")
+		} else {
+			print("Invalid account type")
+		}
+	}
+
+	func withdrawAmount(amount: Float) {
+		if (self.accountType == "CA" || self.accountType == "SA") {
+			transactionCount += 1
+			currentAmount -= amount
+			transactions += "\nTransaction\(transactionCount)   Withdrawal      \(amount)"
+
+			print("\nDear \(self.name), you have withdrawed Rs. \(amount).\nThe avilable balance is Rs. \(currentAmount)")
+		} else {
+			print("\nCan not withdraw money from \(self.accountType) account ")
+		}
+	}
 }
 
 var person1 = Account(name: "Megha", initialAmount: 10000.0)
@@ -157,3 +186,7 @@ person2.accountStatement()
 
 var totalNumberOfAccount = manager1.listOfAccounts.count
 print("Total number of accounts in the bank: \(totalNumberOfAccount)")
+
+manager1.depositeAmount(account: person1, amount:1000)
+person1.accountStatement()
+
